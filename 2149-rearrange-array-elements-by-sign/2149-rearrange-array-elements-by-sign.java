fda9a952d13b1b2aecc17fca73d0929b8c39616a
RainbowJaveline <nealmudaliar70@gmail.com>
2026-07-03 23:04:23 +0530

    Time: 4 ms (41.33%), Space: 175.7 MB (15.64%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -4,7 +4,7 @@ class Solution {
         int pos = 0;
         int neg = 1;
         for (int i = 0; i < arr.length; i++) {
-            if(pos<nums.length-1 && nums[i]>=1){
+            if(nums[i]>=1){
                 arr[pos] = nums[i];
                 pos = pos+2;
             }else{
