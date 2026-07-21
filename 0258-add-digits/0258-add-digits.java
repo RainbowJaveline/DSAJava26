b963b911ade27ccffcda44bef14cff5cd9b6d44d
RainbowJaveline <nealmudaliar70@gmail.com>
2026-07-21 13:08:09 +0530

    Time: 1 ms (97.96%), Space: 42.4 MB (80.32%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,15 +1,10 @@
 class Solution {
     public int addDigits(int num) {
-        while(num > 9){
-            int sum = 0;
-            while(num != 0){
-                int prevSum = sum;
-                int lastDigit = num%10;
-                num = num / 10;
-                sum+= lastDigit;
-            }
-            num = sum;
-        }
-        return num;
+      if(num==0)
+      return 0;
+      else if (num%9==0)
+      return 9;
+      else
+      return num%9;
     }
 }
