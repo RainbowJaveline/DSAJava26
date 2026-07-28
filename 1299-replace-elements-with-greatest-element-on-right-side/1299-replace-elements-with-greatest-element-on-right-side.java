f174fc0dc004f345330f1762b29169dd8652a28e
RainbowJaveline <nealmudaliar70@gmail.com>
2026-07-29 00:27:15 +0530

    Time: 918 ms (26.99%), Space: 49.1 MB (51.2%) - LeetHub

---
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] replaceElements(int[] arr) {
+        int n = arr.length;
+        // if(n == 1) return new int[]{-1};
+        for(int i = 0 ; i< n ; i++){
+            int ele = arr[i];
+            int j = i+1;
+            int max = Integer.MIN_VALUE;
+            while(j<arr.length){
+                max = Math.max(max , arr[j]);
+                j++;
+            }
+            arr[i] = max;
+        }
+        arr[n - 1] = -1;
+        return arr;
+    }
+}
