7392fd11c404d37761add77c71518462627ff20c
RainbowJaveline <nealmudaliar70@gmail.com>
2026-07-30 20:23:11 +0530

    Time: 1 ms (59.37%), Space: 47.8 MB (45.44%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,30 +1,29 @@
 class Solution {
     public int trap(int[] height) {
         int total = 0;
-        int[] leftMax = prefixMax(height);
-        int[] rightMax = suffixMax(height);
-        for(int i=0 ; i<height.length ; i++){
-           if(height[i] < leftMax[i] && height[i] < rightMax[i]){
-             total += Math.min(leftMax[i],rightMax[i]) - height[i];
-           }
-        }
-        return total;
-    }
+        int lMax = 0;
+        int rMax = 0;
+        int l = 0;
+        int r = height.length-1;
+        int n = height.length;
+        while(l<r){
+            if(height[l] <= height[r]){
+                if(lMax > height[l]){
+                    total+= lMax - height[l];
+                }else{
+                    lMax = height[l];
+                }
+                l = l + 1;
+            }else{
+                 if(rMax > height[r]){
+                    total+= rMax - height[r];
+                 }else{
+                    rMax = height[r];
+                 }
+                r = r - 1;
+            }
 
-    public static int[] prefixMax(int[] height){
-        int[] pM = new int[height.length];
-        pM[0] = height[0];
-        for(int i = 1 ; i< pM.length ; i++){
-            pM[i] = Math.max(pM[i-1] , height[i]);
-        }
-        return pM;
-    }
-    public static int[] suffixMax(int[] height){
-        int[] sM = new int[height.length];
-        sM[height.length-1] = height[height.length-1];
-        for(int i = height.length-2 ; i>= 0 ; i--){
-            sM[i] = Math.max(sM[i+1] , height[i]);
-        }
-        return sM;
+        }   
+        return total; 
     }
 }
