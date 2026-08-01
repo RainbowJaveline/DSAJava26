ffb13c94aaafcf9cb40e8338941d4f797dfc342d
RainbowJaveline <nealmudaliar70@gmail.com>
2026-08-01 10:51:17 +0530

    Time: 1 ms (99.98%), Space: 46 MB (97.92%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,32 +1,16 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        // HashMap<Character,Integer> ransom = new HashMap<>();
-        // HashMap<Character,Integer> maga = new HashMap<>();
-        // for(char ch : ransomNote.toCharArray()){
-        //     ransom.put(ch , ransom.getOrDefault(ch,0)+1);
-        // }
-        // for(char ch : magazine.toCharArray()){
-        //     maga.put(ch , maga.getOrDefault(ch,0)+1);
-        // }
-        // int length = ransomNote.length() > magazine.length() ? magazine.length() : ransomNote.length();
-        
-        //return magazine.indexOf(ransomNote) == -1 ? false : true;
-        int i = 0;
-        int j = 0;
-        char[] ran = ransomNote.toCharArray();
-        char[] maga = magazine.toCharArray();
-        Arrays.sort(ran);
-        Arrays.sort(maga);
-        while(j < maga.length && i < ran.length){
-            while((j < maga.length && i < ran.length) && (ran[i] == maga[j])){
-                    i++;
-                    j++;
-                    if(i == ransomNote.length()){
-                        return true;
-                    }
-            }
-            j++;
+        int[] count = new int[26];
+        for(char ch : magazine.toCharArray()){
+            count[ch-'a']++;
         }
-        return false;
+
+        for(char ch : ransomNote.toCharArray()){
+            count[ch-'a']--;
+            if(count[ch-'a'] < 0){
+                return false;
+            }
+        }
+        return true;
     }
 }
