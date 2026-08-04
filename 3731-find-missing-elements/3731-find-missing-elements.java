0225b4ab4e2518b80b6d98539651f3f17d5a3a21
RainbowJaveline <nealmudaliar70@gmail.com>
2026-08-05 00:12:11 +0530

    Time: 1 ms (100%), Space: 46.8 MB (53.61%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.java
+++ 3731-find-missing-elements/3731-find-missing-elements.java
@@ -2,19 +2,20 @@ class Solution {
     public List<Integer> findMissingElements(int[] nums) {
         int smallest = Integer.MAX_VALUE;
         int largest = Integer.MIN_VALUE;
-        List<Integer> list = new ArrayList<>();
         for(int i=0; i<nums.length ; i++){
-            list.add(nums[i]);
             largest = Math.max(largest,nums[i]);
             smallest = Math.min(smallest,nums[i]);
         }
         List<Integer> ans = new ArrayList<>();
-        for(int i=smallest ; i<= largest ; i++){
-            if(!list.contains(i)){
-                ans.add(i);
-            }
+        int[] arr = new int[largest+1];
+        for(int i : nums){
+            arr[i]++;
         }
-
+        for(int i=smallest; i<= largest;i++){
+           if(arr[i] == 0){
+            ans.add(i);
+           }
+        }
         return ans;
     }
 }
