8a98fcf2932a731735a0f45dff312731808ef8f5
RainbowJaveline <nealmudaliar70@gmail.com>
2026-08-17 15:44:44 +0530

    Time: 6 ms (0.84%), Space: 46.6 MB (74.39%) - LeetHub

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+       List<List<Integer>> list = new LinkedList<List<Integer>>();
+        Queue<TreeNode> q = new LinkedList<>();
+        if(root == null){
+            return 0;
+        }
+        int count = 0;
+        q.offer(root);
+        while(!q.isEmpty()){
+            int levelNum = q.size();
+            List<Integer> subList = new LinkedList<>();
+            for(int i= 0 ; i<levelNum ; i++){
+                if(q.peek().left != null) q.offer(q.peek().left);
+                if(q.peek().right != null) q.offer(q.peek().right);
+                subList.add(q.poll().val);
+            }
+            count++;
+            list.add(subList);
+        }
+
+        return count; 
+    }
+}
