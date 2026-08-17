a115aea483add98519b9a35adb80536979e98e19
RainbowJaveline <nealmudaliar70@gmail.com>
2026-08-17 16:13:46 +0530

    Time: 0 ms (100%), Space: 45.8 MB (23.29%) - LeetHub

---
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        int result = heightTree(root);
+        return result != -1;
+    }
+
+    public int heightTree(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+
+        int lh = heightTree(root.left);
+        int rh = heightTree(root.right);
+        if(lh == -1 || rh == -1) return -1;
+        if(Math.abs(rh - lh) > 1) return -1;
+        return Math.max(lh,rh) + 1;
+    }
+}
