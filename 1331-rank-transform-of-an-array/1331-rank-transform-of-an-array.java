c2055bfe804fca67ee92a6e3cd661e35becab7fa
RainbowJaveline <nealmudaliar70@gmail.com>
2026-08-30 18:10:21 +0530

    Time: 28 ms (92.21%), Space: 77 MB (24.66%) - LeetHub

---
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+        int[] temp = new int[arr.length];
+        for(int i = 0 ; i<temp.length ; i++){
+            temp[i] = arr[i];
+        }
+        Arrays.sort(temp);
+        HashMap<Integer , Integer> map =  new HashMap<>();
+        int idx = 0;
+        for(int i=0 ; i<arr.length ; i++){
+            if(i > 0 && temp[i] == temp[i-1]){
+                continue;
+            }
+            map.put(temp[i] , idx++);
+        }
+        int[] ans = new int[temp.length];
+        for(int i= 0 ; i<arr.length ; i++){
+            ans[i] = map.get(arr[i])+1;
+        }
+        return ans;
+    }
+}
